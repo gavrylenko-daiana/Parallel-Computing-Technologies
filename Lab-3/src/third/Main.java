a9ed7725632a58dcacdf4a722c4264479230425d
Daiana Gavrylenko <g-dayana@ukr.net>
2025-04-27 12:09:55 +0200

    updated third task for the 3 lab

--- Lab-3/src/third/Main.java
+++ Lab-3/src/third/Main.java
@@ -7,18 +7,17 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    private static final String TARGET = "go";
+    private static final String TARGET = "same";
 
     public static void main(String[] args) throws IOException {
         String[] files = {
-                "sample-1MB.txt",
-                "sample-10MB.txt",
-                "sample-100MB.txt"
+                "The_freedom_of_the_seas.txt",
+                "The_Minute_Boys_of_Philadelphia.txt"
         };
 
         System.out.printf("Word: \"%s\"%n", TARGET);
-        System.out.printf("%-20s %10s %10s %10s %10s %10s%n",
-                "File", "SeqCnt", "ParCnt", "T_seq", "T_par", "Speed-up");
+        System.out.printf("%-40s %10s %10s %10s %10s %10s%n",
+                "File", "SeqCnt", "ParCnt", "T_seq(ms)", "T_par(ms)", "Speed-up");
 
         for (String file : files) {
             String text = Files.readString(Path.of(file));
@@ -40,7 +39,7 @@ public class Main {
             double tPar = (t4 - t3) / 1e6;
             double speedup = tSeq / tPar;
 
-            System.out.printf("%-20s %10d %10d %10.2f %10.2f %10.2f%n",
+            System.out.printf("%-40s %10d %10d %10.2f %10.2f %10.2f%n",
                     file, seqCount, parCount, tSeq, tPar, speedup);
         }
     }
